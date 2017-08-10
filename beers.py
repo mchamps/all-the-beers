9e300b4f5e3282172b85092ea1e55fc8018e8266
Macbook Pro <17129739+mchamps@users.noreply.github.com>
2017-08-09 23:53:10 -0500

    Swapped comment out after last commit

--- beers.py
+++ beers.py
@@ -9,8 +9,8 @@ data = r_beers['data']
 numberOfItems = r_beers['totalResults']
 
 #uncomment the next line for testing purposes
-for current_page in range(1, 10 + 1):
-#for current_page in range(1, num_pages + 1): 
+#for current_page in range(1, 10 + 1):
+for current_page in range(1, num_pages + 1): 
     r_beers = requests.get("http://api.brewerydb.com/v2/beers?key=API_KEY_HERE&withBreweries=Y&p=" + str(current_page)).json()
     data.append(r_beers['data'])
     print str(current_page) + " of " + str(num_pages) 

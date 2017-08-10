7769b07d958c13bd853c6207899103124dcc73ed
Macbook Pro <17129739+mchamps@users.noreply.github.com>
2017-08-09 23:54:23 -0500

    Moved data.append into loop

--- brewerys.py
+++ brewerys.py
@@ -1,20 +1,19 @@
 import requests
 import json
 
-r_brewerys = requests.get("http://api.brewerydb.com/v2/locations?key=API_KEY_HERE&p=1").json()
-num_pages = r_brewerys['numberOfPages']
-current_page = r_brewerys['currentPage']
+r_beers = requests.get("http://api.brewerydb.com/v2/locations?key=API_KEY_HERE&p=").json()
+num_pages = r_beers['numberOfPages']
+current_page = r_beers['currentPage']
 
-data = r_brewerys['data']
-numberOfItems = r_brewerys['totalResults']
+data = r_beers['data']
+numberOfItems = r_beers['totalResults']
 
 #uncomment the next line for testing purposes
-#for current_page in range(1, 4):
-for current_page in range(1, num_pages): 
-    r_brewerys = requests.get("http://api.brewerydb.com/v2/locations?key=API_KEY_HERE&p=" + str(current_page)).json()
-    print current_page
-
-data.append(r_brewerys['data'])
+#for current_page in range(1, 10 + 1):
+for current_page in range(1, num_pages + 1): 
+    r_beers = requests.get("http://api.brewerydb.com/v2/locations?key=API_KEY_HERE&p=" + str(current_page)).json()
+    data.append(r_beers['data'])
+    print str(current_page) + " of " + str(num_pages) 
 
 with open('data.json', 'w') as outfile:
     json.dump(data, outfile)

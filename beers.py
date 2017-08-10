85a4dace5bf30bd0b3e6b1e2599f82ad5ba12aa8
Macbook Pro <macbookpro@macbook.local>
2017-08-09 22:18:40 -0500

    Added pagination iteration to the request

--- beers.py
+++ beers.py
@@ -1,16 +1,20 @@
 import requests
 import json
 
-r_beers = requests.get("http://api.brewerydb.com/v2/beers?key=API_KEY_HERE&withBreweries=Y&withIngredients=Y").json()
+r_beers = requests.get("http://api.brewerydb.com/v2/beers?key=API_KEY_HERE&withBreweries=Y&withIngredients=Y&p=1").json()
 num_pages = r_beers['numberOfPages']
+current_page = r_beers['currentPage']
 
 data = r_beers['data']
 numberOfItems = r_beers['totalResults']
 
-for page in range(2, num_pages+1):
-#for page in range(2, 4):
-    r_beers = requests.get("http://api.brewerydb.com/v2/beers?key=API_KEY_HERE&withBreweries=Y&withIngredients=Y", params={'currentPage': page}).json()
-    data.append(r_beers['data'])
+#uncomment the next line for testing purposes
+#for current_page in range(1, 4):
+for current_page in range(1, num_pages): 
+    r_beers = requests.get("http://api.brewerydb.com/v2/beers?key=API_KEY_HERE&withBreweries=Y&withIngredients=Y&p=" + str(current_page)).json()
+    print current_page
+
+data.append(r_beers['data'])
 
 with open('data.json', 'w') as outfile:
     json.dump(data, outfile)
